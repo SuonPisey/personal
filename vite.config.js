174864b2ce778baa10b2cbdad6c3333776661d05
suonpisey01778 <pisey.suon@cubetiqs.com>
2024-11-21 23:12:14 +0700

    last

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Set base to root
+  base: '/react-portfolio-template/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        silenceDeprecations: ['mixed-decls'], // Ensure SCSS warnings are suppressed
+        silenceDeprecations: ['mixed-decls'],
       },
     },
   },
-});
+})
